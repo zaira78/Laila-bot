4ddba75f6a7b72bf0946451642ccc92f15476093
zaira78 <innocentadnan256@gmail.com>
2025-07-04 23:36:47 +0500

    Update French.js

--- Priyansh/commands/French.js
+++ Priyansh/commands/French.js
@@ -12,9 +12,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("French")==0 || event.body.indexOf("FRENCH")==0 || event.body.indexOf("french")==0 || event.body.indexOf(".french")==0) {
+	if (event.body.indexOf("French fries")==0 || event.body.indexOf("Fries")==0 || event.body.indexOf("FRIES")==0 || event.body.indexOf("FRENCH FRIES")==0) {
 		var msg = {
-				body: "💝 YE LO BABY FRENCH KHAO🍟\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 ཫ༄𒁍⃝𝐌𝐀𝐑⃟𝐈𝐍𝐀❤️-)",
+				body: "💝 YE LO BABY FRENCH FRIES KHAO🍟\n\n(-❤️-)",
 				attachment: 
 fs.createReadStream(__dirname + `/cache/French.jpg`)
 			}
