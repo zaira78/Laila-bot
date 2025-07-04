44d5173a7734f01378391540eecdbf0eb195f54d
zaira78 <innocentadnan256@gmail.com>
2025-07-05 00:00:48 +0500

    Update Murga.js

--- Priyansh/commands/Murga.js
+++ Priyansh/commands/Murga.js
@@ -12,9 +12,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("CHICKEN")==0 || event.body.indexOf("Murga")==0 || event.body.indexOf("Chiken")==0 || event.body.indexOf("Chicken")==0) {
+	if (event.body.indexOf("CHICKEN")==0 || event.body.indexOf("Murga")==0 || event.body.indexOf("Chicken")==0 || event.body.indexOf("Chicken")==0) {
 		var msg = {
-				body: "💝 YE LO BABY CHICKEN KHAO 🐔\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 ཫ༄𒁍⃝𝐌𝐀𝐑⃟𝐈𝐍𝐀❤️-)",
+				body: "💝 YE LO BABY CHICKEN KHAO 🐔\n\n(-❤️-)",
 				attachment: 
 fs.createReadStream(__dirname + `/cache/chicken.jpg`)
 			}
