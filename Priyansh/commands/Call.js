39ca3135ca5c02d4d79e673d3d7f7cad2582e405
zaira78 <innocentadnan256@gmail.com>
2025-07-04 22:44:32 +0500

    Update Call.js

--- Priyansh/commands/Call.js
+++ Priyansh/commands/Call.js
@@ -13,7 +13,7 @@ module.exports.handleEvent = async function({ api, event, client, Users, __GLOBA
   var name = await Users.getNameUser(event.senderID);
   if (event.body.indexOf("call aa")>=0 || event.body.indexOf("Call a")>=0 || event.body.indexOf("CALL")>=0 || event.body.indexOf("Bot call")>=0 || event.body.indexOf("CLL")>=0 || event.body.indexOf("CL")>=0 || event.body.indexOf("Call aao")>=0 || event.body.indexOf("Call tapko")>=0 || event.body.indexOf("call tapko")>=0 || event.body.indexOf("Call bot")>=0 ) { 
     var msg = {
-        body: ` ${name} यार मैं कैसे कॉल आऊं मैं तो बोट हूं।🥺🥺👈`
+        body: ` ${name} 𝗬𝗮𝗿 𝗺𝗲 𝗸𝗲𝘀𝗲 𝗰𝗮𝗹𝗹 𝗮𝗼𝘂𝗻 𝗺𝗲 𝘁𝗼 𝗯𝗼𝘁 𝗵𝘂 𝗻𝗮🥺🥺👈`
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("📞", event.messageID, (err) => {}, true)
