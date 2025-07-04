aca4a07d2a4688f196a508a88269c6094a5383ef
zaira78 <innocentadnan256@gmail.com>
2025-07-04 22:18:29 +0500

    Update 0uid.js

--- Priyansh/commands/0uid.js
+++ Priyansh/commands/0uid.js
@@ -12,12 +12,12 @@ module.exports.config = {
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   let react = event.body.toLowerCase();
-  if(react.includes("🧐") ||
+  if(react.includes("😋") ||
      react.includes("😯") || react.includes("🤪") || react.includes("😳") ||
 react.includes("🙀") ||
 react.includes("😱")) {
     var msg = {
-        body: `【  𝐀𝐈𝐒𝐄 𝐌𝐀𝐓 𝐃𝐄𝐊𝐇0 𝐘𝐀𝐑 𝐍𝐀𝐇𝐈 𝗧𝗢 𝐊𝐈𝐒𝐒  𝐊𝐀𝐑 𝐋𝐔𝐍𝐆𝐀______😁😁😝】`,
+        body: `【  𝐀𝐈𝐒𝐄 𝐌𝐀𝐓 𝐃𝐄𝐊𝐇0 𝐘𝐀𝐑 𝐍𝐀𝐇𝐈 𝗧𝗢 𝐊𝐈𝐒𝐒  𝐊𝐀𝐑 𝐋𝐔𝐍𝐆𝗜______😁😁😝】`,
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😅", event.messageID, (err) => {}, true)
