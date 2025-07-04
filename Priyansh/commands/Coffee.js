136a9b88a03658a1c8a47dbe22265758b6a220a0
zaira78 <innocentadnan256@gmail.com>
2025-07-04 23:32:06 +0500

    Update Coffee.js

--- Priyansh/commands/Coffee.js
+++ Priyansh/commands/Coffee.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("Coffee")==0 || event.body.indexOf("coffee")==0 || event.body.indexOf("COFFEE ")==0 || event.body.indexOf("cofe")==0) {
 		var msg = {
-				body: "🙂𝐘𝐄 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐂𝐎𝐅𝐅𝐄𝐄🙂🤟\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 ཫ༄𒁍⃝𝐌𝐀𝐑⃟𝐈𝐍𝐀❤️-)",
+				body: "🙂𝐘𝐄 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐂𝐎𝐅𝐅𝐄𝐄🙂🤟\n\n(-❤️-)",
 				attachment: fs.createReadStream(__dirname + `/cache/Coffee.gif`)
 			}
 			api.sendMessage(msg, threadID, messageID);
