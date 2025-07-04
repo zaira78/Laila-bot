befa25add866a6aef284e3d371c874d7341945d4
zaira78 <innocentadnan256@gmail.com>
2025-07-04 19:08:18 +0500

    Update ##.js

--- Priyansh/commands/##.js
+++ Priyansh/commands/##.js
@@ -68,9 +68,9 @@ var callback = () => api.sendMessage({body:`➢ Admin and Bot Information
 
 ⁂ Bot Name: ${global.config.BOTNAME}
 ✧ Bot Admin: ${global.config.ADMINBOT}
-♛ Bot Admin Link: https://www.facebook.com/MR.AARYAN.HERE
+♛ Bot Admin Link: https://www.facebook.com/callmebebo77
 ❂ Bot Prefix: ${global.config.PREFIX}
-✫ Bot Owner: https://www.facebook.com/MR.AARYAN.HERE
+✫ Bot Owner: https://www.facebook.com/callmebebo77
 ➟ UPTIME
 ✬ Today is: ${juswa} 
 
