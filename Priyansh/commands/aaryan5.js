a2b3693247025c20c23861651cd9938ba462795d
zaira78 <innocentadnan256@gmail.com>
2025-07-05 05:36:35 +0500

    Update aaryan5.js

--- Priyansh/commands/aaryan5.js
+++ Priyansh/commands/aaryan5.js
@@ -13,11 +13,11 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
 	if(react.includes("😒") ||
-     react.includes("🙈") || react.includes("🥺") || react.includes("😗") ||
+     react.includes("😒") || react.includes("😒") || react.includes("😗") ||
 react.includes("🙄") ||
-react.includes("😙")) {
+react.includes("🙄")) {
 		var msg = {
-				body: `𝐀𝐖𝐖 𝐌𝐄𝐋𝐀 𝐁𝐀𝐁𝐔 𝐒𝐇𝐎𝐍𝐀 𝐉𝐀𝐃𝐔 𝐓𝐎𝐍𝐀 𝐍𝐀 𝐑𝐎𝐍𝐀 𝐍𝐀 𝐃𝐇𝐎𝐍𝐀 𝐍𝐀 𝐒𝐎𝐍𝐀`,
+				body: `𝐀𝐖𝐖 𝐌𝐄𝐋𝐀 𝐁𝐀𝐁𝐔 𝐒𝐇𝐎𝐍𝐀 𝐉𝐀𝐃𝐔 𝐓𝐎𝐍𝐀🥺👀`,
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😒", event.messageID, (err) => {}, true)
