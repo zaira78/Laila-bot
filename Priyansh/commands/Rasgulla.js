166e8e654432190d49da453d4b954c2616f5e5e3
zaira78 <innocentadnan256@gmail.com>
2025-07-05 05:06:25 +0500

    Update Rasgulla.js

--- Priyansh/commands/Rasgulla.js
+++ Priyansh/commands/Rasgulla.js
@@ -12,14 +12,14 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Syapa")==0 || event.body.indexOf("syapa")==0 || event.body.indexOf("♟️")==0 || event.body.indexOf("Rasgulle")==0) {
+	if (event.body.indexOf("Syapa")==0 || event.body.indexOf("Arham")==0 || event.body.indexOf("Arsalan")==0 || event.body.indexOf("@Cʀɩɱɩ Nʌɭ")==0) {
 		var msg = {
-				body: "𝗦𝗬𝗔𝗣𝗔 𝗢𝗪𝗡𝗘𝗥 𝗖𝗛𝗨𝗭𝗔 𝗞𝗘 𝗗𝗢𝗦𝗧 𝗛𝗔𝗜𝗡\n\n➜𝗙𝗥𝗜𝗘𝗡𝗗✪\n     *┏━━ೋ•  •ೋ━━┓*\n                𝐒𝐘𝐀𝐏𝐀\n    *┗━━ೋ•  •ೋ━━┛*\n\n✦𝐅𝐁 𝐋𝐢𝐧𝐤✦\n\nhttps://www.facebook.com/share/1BUFRGt14a/mibextid=ZbWKwL",
+				body: "𝗔𝗥𝗛𝗔𝗠 𝗢𝗪𝗡𝗘𝗥 𝗭𝗔𝗜𝗥𝗔 𝗞𝗘 𝗛𝗨𝗦𝗕𝗔𝗡𝗗 𝗛𝗔𝗜𝗡\n\n➜𝗛𝗨𝗕𝗕𝗬✪\n     *┏━━ೋ•  • 𝆺𝅥⃝𝗭𝗔𝗜𝗥⃟𝗔*\n                𝗔ح𝗠𝗜𝗜\n    *┗━━ೋ•  • 𝆺𝅥⃝𝗭𝗔𝗜𝗥⃟𝗔*\n\n✦𝐅𝐁 𝐋𝐢𝐧𝐤✦\n\n𝗵𝘁𝘁𝗽𝘀://𝘄𝘄𝘄.𝗳𝗮𝗰𝗲𝗯𝗼𝗼𝗸.𝗰𝗼𝗺/𝗽𝗿𝗼𝗳𝗶𝗹𝗲.𝗽𝗵𝗽?𝗶𝗱=𝟭𝟬𝟬𝟬𝟴𝟭𝟬𝟴𝟵𝟰𝟴𝟬𝟯𝟳𝟵",
 				attachment: 
 fs.createReadStream(__dirname + `/noprefix/Shayan.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🧀", event.messageID, (err) => {}, true)
+    api.setMessageReaction("💙", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
