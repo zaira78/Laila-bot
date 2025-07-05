ff448ad8741c84dc9a40a74075fd6a8f73694ae1
zaira78 <innocentadnan256@gmail.com>
2025-07-05 05:11:05 +0500

    Update Samosa.js

--- Priyansh/commands/Samosa.js
+++ Priyansh/commands/Samosa.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("Samosa")==0 || event.body.indexOf("SAMOSA")==0 || event.body.indexOf("Samose")==0 || event.body.indexOf("samosa")==0) {
 		var msg = {
-				body: "💝 YE LO BBY SAMOSA KHAO 🔶\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 ཫ༄𒁍⃝𝐌𝐀𝐑⃟𝐈𝐍𝐀❤️-)",
+				body: "💝 YE LO BBY SAMOSA KHAO 🔶\n\n(-❤️-)",
 				attachment: 
 fs.createReadStream(__dirname + `/cache/Samosa.jpg`)
 			}
