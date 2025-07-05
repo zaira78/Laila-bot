b44766c7acde3a93badc53cdc623a1329649f52d
zaira78 <innocentadnan256@gmail.com>
2025-07-05 21:47:12 +0500

    Update motka.js

--- Priyansh/commands/motka.js
+++ Priyansh/commands/motka.js
@@ -11,12 +11,12 @@ module.exports.config = {
 module.exports.handleEvent = async function({ api, event, client, Users, __GLOBAL }) {
   var { threadID, messageID } = event;
   var name = await Users.getNameUser(event.senderID);
-  if (event.body.indexOf("KUTTA")>=0 || event.body.indexOf("KUTTA")>=0 || event.body.indexOf("Kutte")>=0 || event.body.indexOf("Kute")>=0 || event.body.indexOf("kutte")>=0 || event.body.indexOf("kute")>=0 || event.body.indexOf("kutta")>=0 || event.body.indexOf("kuta")>=0 || event.body.indexOf("Kuta")>=0 || event.body.indexOf("kutta")>=0 ) { 
+  if (event.body.indexOf("KUTTA")>=0 || event.body.indexOf("Kutta")>=0 || event.body.indexOf("Kutte")>=0 || event.body.indexOf("Kutte")>=0 || event.body.indexOf("kutte")>=0 || event.body.indexOf("kute")>=0 || event.body.indexOf("kutta")>=0 || event.body.indexOf("kuta")>=0 || event.body.indexOf("Kuta")>=0 || event.body.indexOf("kutta")>=0 ) { 
     var msg = {
-        body: `  ${name} तुम होगी कुत्ती मै तो बोट हु 😒😝👈 `
+        body: `  ${name} Tu kutta me to bot hu😝 `
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🐃", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🐶", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = function({ api, event, client, __GLOBAL }) {
