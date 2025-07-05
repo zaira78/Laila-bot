f303a121874b9a1b1b75f3efcbcdf874bfa394a6
zaira78 <innocentadnan256@gmail.com>
2025-07-05 21:37:09 +0500

    Update help2.js

--- Priyansh/commands/help2.js
+++ Priyansh/commands/help2.js
@@ -67,7 +67,7 @@ module.exports. run = function({ api, event, args, getText }) {
     for (let item of returnArray) msg += `「 ${++i} 」${prefix}${item}\n`;
     
     
-    const siu = `Command list 📄\nMade by ཫ༄𒁍≛⃝𝗠𝗔𝗥⃟𝗜𝗡𝗔🍒🥀 🥀\nFor More Information type /help (command name) ✨`;
+    const siu = `Command list 📄\nMade by ཫ༄𒁍𝆺𝅥⃝𝗭𝗔𝗜𝗥⃟𝗔🍒🥀 🥀\nFor More Information type /help (command name) ✨`;
     
  const text = `\nPage (${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)})`;
  
